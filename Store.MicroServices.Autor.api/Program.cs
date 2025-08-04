2fe584d927e6d01ed81e2b81feb15eb774c11ed1
DanielTrujilloTapia <22300119@uttt.edu.mx>
2025-08-04 13:42:15 -0600

    el swagger se saco del if de desarrollo para poder verlo en produccion

--- Store.MicroServices.Autor.api/Program.cs
+++ Store.MicroServices.Autor.api/Program.cs
@@ -96,11 +96,8 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowAll");
 
